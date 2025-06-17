ac3ed50ac6682a434e6b398924b9afb5cad72b3c
Vicky <vikaspurohit105>
2025-06-18 05:05:47 +0530

    4rd

--- assignmentvr/settings.py
+++ assignmentvr/settings.py
@@ -1,4 +1,4 @@
-
+import dj_database_url
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -69,17 +69,20 @@ CORS_ALLOW_CREDENTIALS = True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'assignment',            # your database name
-        'USER': 'root',          # your database user (or 'root')
-        'PASSWORD': 'om@123',     # your password
-        'HOST': 'localhost',               # or '127.0.0.1'
-        'PORT': '3306',                    # default MySQL port
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'assignment',            # your database name
+#         'USER': 'root',          # your database user (or 'root')
+#         'PASSWORD': 'om@123',     # your password
+#         'HOST': 'localhost',               # or '127.0.0.1'
+#         'PORT': '3306',                    # default MySQL port
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 
 # Password validation
