25fd902091061ccd3154da944f6b1b502b4c0f8d
Vicky <vikaspurohit105>
2025-07-02 00:57:47 +0530

    new ush

--- assignmentvr/settings.py
+++ assignmentvr/settings.py
@@ -10,7 +10,11 @@ SECRET_KEY = 'django-insecure-s2$nh-+all6oc5sgd59b&r@_c=_383wy3#7#wngfl1-tiw+@@l
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['assignment-2sc3.onrender.com']
+ALLOWED_HOSTS = [    
+                 'assignment-2sc3.onrender.com',
+                 'localhost',
+                 '127.0.0.1',
+    ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
